4d31ea0291e48e6b5a5fbe071ef6e268faff210a
Maksym bc2024-3 <max.murtazin2006@gmail.com>
2024-11-13 19:26:22 +0200

    Creating my own map function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const { Command } = require('commander');
 const path = require('path');
-
+//Створив екземпляр Command для обробки аргументів.
 const program = new Command();
 program
   .option('-i, --input <path>', 'path to input file (JSON data)')
@@ -9,7 +9,6 @@ program
   .option('-d, --display', 'display the result in the console');
 
 program.parse(process.argv);
-
 const options = program.opts();
 
 if (!options.input) 
@@ -17,7 +16,7 @@ if (!options.input)
 console.error('Please, specify input file');
 process.exit(1);
 }
-
+// Отримання шляху до файлу
 const inputFilePath = path.resolve(options.input);
 let data;
 try 
@@ -39,12 +38,22 @@ console.error('Error parsing JSON');
 process.exit(1);
 }
 
-const results = jsonData.map(item => {
+function customMap(array, callback) {
+  const result = [];
+  for (let i = 0; i < array.length; i++) {
+    result.push(callback(array[i], i, array));
+  }
+  return result;
+}
+
+const Transformation = (item) => {
   const stockCode = item.StockCode || 'N/A';
   const valCode = item.ValCode || 'N/A';
   const attraction = item.Attraction || 'N/A';
   return `${stockCode}--${valCode}--${attraction}`;
-}).join('\n');
+}
+
+const results = customMap(jsonData, Transformation).join('\n');
 
   if (options.display) {console.log(results);}
 
@@ -54,5 +63,4 @@ if (options.output)
   try 
   {
   fs.writeFileSync(outputFilePath, results);
-  } catch (err) { process.exit(1);}
-  }
+  } catch (err) { process.exit(1);}}
